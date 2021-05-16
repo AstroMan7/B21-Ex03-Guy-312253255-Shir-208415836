ab60d4798bebbd61d45e1612bbd99d97cc2e1bd7
AstroMan7 <hassinguy@gmail.com>
2021-05-16 13:45:24 +0300

    Add project files.

---
+++ Ex03.ConsoleUI/GarageUserInteface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+	public class GarageUserInteface
+	{
+		private readonly Menu r_StartMenu = new Menu();
+		private readonly Menu r_VehicleMenu = new Menu();
+
+		public enum eUserChoice
+		{
+			AddNewVehcile = 1,
+			ShowVehcilesList,
+			ChangeVehicleStatus,
+			AddAirPresuer,
+			AddFuel,
+			ChargeBattery,
+			ShowVehicleData,
+			Exit,
+		}
+
+		public void RunProgram()
+		{
+			int userChoice = 0;
+
+			Console.WriteLine(
+@"Hey and welcome to ^^^Hassin & Savion Garage^^^
+Please select one of the option below to get things moving:" + Environment.NewLine);
+			r_StartMenu.SetStartMenu();
+			r_VehicleMenu.SetVehicleMenu();
+		}
+	}
+}
---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+	public abstract class Vehicle
+	{
+		public enum eVehicleStatus
+		{
+			InRepair = 1,
+			Repaired,
+		    Paid,
+		}
+
+		// Consts
+		public const int k_ModelQLocation = 0;
+		public const int k_OwnerNameQLocation = 1;
+		public const int k_OwnerPhoneQLocation = 2;
+		public const int k_EnergyLeftQLocation = 3;
+		public const int k_WheelManufacturerQLocation = 4;
+		public const int k_WheelAirPressureQLocation = 5;
+
+		// Data members
+		protected string m_LicenseNumber;
+		protected string m_Model;
+		protected string m_OwnerOfVehicle;
+		protected string m_OwnerPhoneNumber;
+		protected eVehicleStatus m_VehicleStatus = eVehicleStatus.InRepair; //automaticly puts first val?
+		protected Engine m_MyEngine;
+		protected float m_EnergyLeft;
+
+		// C'tor
+		protected Vehicle(string i_LicenseNumber, Engine i_Engine)
+		{
+			m_MyEngine = i_Engine;
+			m_LicenseNumber = i_LicenseNumber;
+		}
+		// Get method for m_MyEngine
+		public Engine MyEngine
+		{
+			get
+			{
+				return m_MyEngine;
+			}
+		}
+
+		// Get/Set method for m_VehicleStatus
+		public eVehicleStatus VehicleStatus
+		{
+			get
+			{
+				return m_VehicleStatus;
+			}
+
+			set
+			{
+				m_VehicleStatus = value;
+			}
+
+		}
+	}
+}
